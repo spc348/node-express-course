b3cf7d99337da22a3cda8c119abb9cebcff6ff9e
cahn <spc348@nyu.edu>
2021-03-21 19:30:15 -0400

    add first GET route

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const express = require('express');
 // create instance of express called app
 const app = express();
 
+// mock user data
+const mockUserData=[
+    {name: 'Mark'},
+    {name: 'Jill'}
+]
+
+
+app.get('/users', function(req,res){
+    res.json({
+        success: true,
+        message: 'successfully go users.',
+        users: mockUserData
+    })
+})
+
+
+// run app
 app.listen(8000, function() {
     console.log("server is running");
 })
